75f8d29057b4a0784da9749dbdec0862c26a5395
Lavanya M <aynaval54@gmail.com>
2021-09-16 21:01:24 +0530

    Add files via upload

---
+++ EDA.mm
@@ -0,0 +1,103 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="EDA" FOLDED="false" ID="ID_1639289380" CREATED="1631638038335" MODIFIED="1631639402885" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="15" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Discuss problem statement" POSITION="right" ID="ID_1246100022" CREATED="1631638056354" MODIFIED="1631638696149" HGAP_QUANTITY="25.24999966472388 pt" VSHIFT_QUANTITY="-17.249999485909967 pt">
+<edge COLOR="#ff0000"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="short summery on cols" POSITION="right" ID="ID_924335476" CREATED="1631638154730" MODIFIED="1631638704178" HGAP_QUANTITY="41.74999917298558 pt" VSHIFT_QUANTITY="-26.249999217689062 pt">
+<edge COLOR="#0000ff"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="check for null values" POSITION="right" ID="ID_704053397" CREATED="1631638566821" MODIFIED="1631638707197" HGAP_QUANTITY="49.99999892711642 pt" VSHIFT_QUANTITY="-44.999998658895535 pt">
+<edge COLOR="#007c00"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="Discuss what to do with null values" POSITION="right" ID="ID_39210498" CREATED="1631638682399" MODIFIED="1631638770341" HGAP_QUANTITY="62.74999854713683 pt" VSHIFT_QUANTITY="-35.99999892711642 pt">
+<edge COLOR="#7c007c"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="Discuss about key features in hotel industry" POSITION="right" ID="ID_803790213" CREATED="1631638759171" MODIFIED="1631638872280">
+<edge COLOR="#007c7c"/>
+<font SIZE="16"/>
+<node TEXT="see corelation between cols" ID="ID_1091019565" CREATED="1631639066197" MODIFIED="1631639167676">
+<font SIZE="16"/>
+</node>
+</node>
+<node TEXT="Add new features" POSITION="right" ID="ID_1094276235" CREATED="1631638878815" MODIFIED="1631639402888">
+<edge COLOR="#ff0000"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="discuss libs we can use" POSITION="right" ID="ID_1947100168" CREATED="1631639410275" MODIFIED="1631639432494">
+<edge COLOR="#ff00ff"/>
+<font SIZE="16"/>
+</node>
+<node TEXT="Diving the work" POSITION="right" ID="ID_1566070023" CREATED="1631638934914" MODIFIED="1631638987297">
+<edge COLOR="#0000ff"/>
+<font SIZE="14"/>
+</node>
+</node>
+</map>
